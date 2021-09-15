f49c939b0b4d1229e069591805a6d358218c6747
Павел Васенев <p.vasenev@citrus-soft.ru>
2021-09-15 14:17:40 +0300

    Добавлена миграция с хайлоад блоком "Избранное"

---
+++ local/php_interface/migrations/Version20210915141712.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20210915141712 extends Version
+{
+    protected $description = "Создание хайлоад блока \"Избранное\"";
+
+    protected $moduleVersion = "3.28.7";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $hlblockId = $helper->Hlblock()->saveHlblock(array (
+  'NAME' => 'Favorites',
+  'TABLE_NAME' => 'favorites',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Список избранных записей',
+    ),
+  ),
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_USER_ID',
+  'USER_TYPE_ID' => 'integer',
+  'XML_ID' => 'UF_USER_ID',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'MIN_VALUE' => 0,
+    'MAX_VALUE' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => 'Идентификатор пользователя',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_BLOG_ELEMENT_ID',
+  'USER_TYPE_ID' => 'integer',
+  'XML_ID' => 'UF_BLOG_ELEMENT_ID',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'MIN_VALUE' => 0,
+    'MAX_VALUE' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => 'Идентификатор записи',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
